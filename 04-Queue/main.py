a35a0c0778f5e3bf5be37467c80e782c89834a5c
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-08-26 16:27:36 +0300

    Queue and linled l. modified.

--- 04-Queue/main.py
+++ 04-Queue/main.py
@@ -16,7 +16,34 @@ class Queue:
             print(temp.value)
             temp=temp.next
 
+    def enqueue(self,value):
+        new_node=Node(value)
+        if self.first==None:
+            self.first=new_node
+            self.last=new_node
+        else:
+            self.last.next=new_node
+            self.last=new_node
+        self.length+=1
 
+    def dequeue(self):
+        if self.length==0:
+            return None
+        temp=self.first
+        if self.length==1:
+            self.first=None
+            self.last=None
+        else:
+            self.first=temp.next
+            temp.next=None
+        self.length-=1
+
+        return temp
+        
 my_queue=Queue(1)
-
+my_queue.enqueue(2)
 my_queue.print_queue()
+print("-----------------")
+print(my_queue.dequeue())
+print(my_queue.dequeue())
+print(my_queue.dequeue())
