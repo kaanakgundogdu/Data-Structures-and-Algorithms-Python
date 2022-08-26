ce0c82763f97dbe08448d39f3786d4e871873f4f
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-08-26 16:12:44 +0300

    Queue added.

--- 03-Stack/main.py
+++ 03-Stack/main.py
@@ -9,7 +9,7 @@ class Stack:
         self.top=new_node
         self.height=1
 
-    def print_list(self):
+    def print_stack(self):
         temp=self.top
         while temp is not None:
             print(temp.value)
@@ -37,6 +37,6 @@ class Stack:
 my_stack=Stack(1)
 my_stack.push(0)
 my_stack.push(2)
-my_stack.print_list()
+my_stack.print_stack()
 print("-------------")
 print(my_stack.pop().value)
